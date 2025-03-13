692eca5be3fb7f0178e40999d30370b3444efe5b
Stefan Dzhestanov <jestanoff@gmail.com>
2025-03-13 22:19:21 +0000

    Fetch comments only when component in viewport

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,14 +1,18 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Button, Card, Typography, TextField, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { Post } from "../../types";
 import { trpcReact } from "@/trpc/trpcReact";
 
 const PostComponent: React.FC<Partial<Post>> = ({ id, authorId, title, content }) => {
+  const [isInView, setIsInView] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
   const [showComments, setShowComments] = useState(false);
   const [newComment, setNewComment] = useState("");
 
-  const { data: comments } = trpcReact.getComment.useQuery({ postId: `${id}` }, { enabled: !!id });
+  const commentsQuery = trpcReact.getComment.useQuery({ postId: `${id}` }, { enabled: !!id && isInView });
+  const { data: comments } = commentsQuery;
   const postCommentMutation = trpcReact.postComment.useMutation();
 
   const handleSubmitComment = useCallback(() => {
@@ -16,6 +20,20 @@ const PostComponent: React.FC<Partial<Post>> = ({ id, authorId, title, content }
     postCommentMutation.mutate({ postId: `${id}`, content: newComment });
     setNewComment("");
   }, [id, newComment, postCommentMutation]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => setIsInView(entry.isIntersecting), { threshold: 0.1 });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   return (
     <Card variant="outlined" sx={{ p: 2, my: 4 }}>
@@ -43,6 +61,7 @@ const PostComponent: React.FC<Partial<Post>> = ({ id, authorId, title, content }
       )}
       <Box sx={{ display: "flex", gap: 1, my: 2 }}>
         <TextField
+          ref={ref}
           fullWidth
           size="small"
           placeholder="Write a comment..."
